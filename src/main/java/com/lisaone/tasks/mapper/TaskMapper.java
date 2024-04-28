d3821ec319df0b6c18f0a0ae7e94b4c3fd4ec238
Soumyakanti Giri <giri.soumyakanti@gmail.com>
2024-04-28 19:46:15 +0530

    edit tasks, date util and check

--- src/main/java/com/lisaone/tasks/mapper/TaskMapper.java
+++ src/main/java/com/lisaone/tasks/mapper/TaskMapper.java
@@ -1,15 +1,22 @@
 package com.lisaone.tasks.mapper;
 
+import com.lisaone.tasks.dto.TaskEditRequest;
 import com.lisaone.tasks.dto.TaskRequest;
 import com.lisaone.tasks.entity.Task;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = org.mapstruct.NullValuePropertyMappingStrategy.IGNORE)
 public abstract class TaskMapper{
 
     public abstract TaskRequest toTaskRequest(Task task);
 
     @Mapping(target = "id", ignore = true)
     public abstract Task toTask(TaskRequest taskRequest);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "priority", ignore = true)
+    public abstract void update(@MappingTarget Task task, TaskEditRequest taskEditRequest);
 }
--- src/main/java/com/lisaone/tasks/repository/TaskRepository.java
+++ src/main/java/com/lisaone/tasks/repository/TaskRepository.java
@@ -4,8 +4,13 @@ import com.lisaone.tasks.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
     @Override
-    public <S extends Task> S save(S entity);
+    <S extends Task> S save(S entity);
+
+    @Override
+    Optional<Task> findById(Long id);
 }
